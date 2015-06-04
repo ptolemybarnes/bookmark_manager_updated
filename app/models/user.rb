e02436ee9e6dcc5ff304939912f3bc335b46e097
DanLDB <dpbergquist>
2015-06-04 15:19:20 +0100

    completed version 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,13 @@ class User
   property :email, String
   property :password_digest, Text
 
+  validates_confirmation_of :password
+
+  attr_reader :password
+  attr_accessor :password_confirmation # NOTE: Never create an accessor with the same name as a DataMapper property
+
   def password=(password)
+    @password = password
     self.password_digest = BCrypt::Password.create(password)
   end
 end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,23 +1,23 @@
 feature 'User signs up' do
 
-  scenario 'with a password that does not match' do
-    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
-  end
-
-
   scenario 'when being a new user visiting the site' do
     expect { sign_up }.to change(User, :count).by(1)
     expect(page).to have_content('Welcome, alice@example.com')
     expect(User.first.email).to eq('alice@example.com')
   end
 
-  def sign_up(email: 'wawa@example.com',
+  scenario 'with a password that does not match' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+  end
+
+  def sign_up(email: 'alice@example.com',
               password: '12345678',
               password_confirmation: '12345678')
     visit '/users/new'
-    fill_in :email, with: user.fetch(email)
+    fill_in :email, with: email
     fill_in :password, with: password
     fill_in :password_confirmation, with: password_confirmation
+    click_button 'Sign up'
   end
-  
+
 end
